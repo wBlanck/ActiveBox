16e5288b5f938784539432c69c6b20326fe3ba14
wblanck <blanck.william@gmail.com>
2021-09-14 14:07:10 +0200

    added hamburger functions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const headerContent = document.querySelector(".header__content");
 const nav = document.querySelector(".nav");
-
+const mobileNav = document.querySelector(".mobile-nav");
+const hamburger = document.querySelector(".hamburger");
 
 
 window.addEventListener("scroll",function() { 
@@ -14,4 +15,15 @@ window.addEventListener("scroll",function() {
     nav.style.padding = "40px 10px";
     headerContent.id = "";
   }
+});
+
+hamburger.addEventListener("click", function() {
+  if(mobileNav.style.visibility != "visible"){
+    mobileNav.style.opacity = "1";
+    mobileNav.style.visibility = "visible"
+  } else {
+    mobileNav.style.opacity = "0";
+    mobileNav.style.visibility = "hidden"
+  }
+  
 });
